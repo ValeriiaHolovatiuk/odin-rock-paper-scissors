08d8b2d5e4473f259da08e7de109a1a2130b08ef
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-09-24 23:58:14 +0200

    Added the logic to show scores + choices in the UI

--- scripts/rockPaperScissors.js
+++ scripts/rockPaperScissors.js
@@ -6,30 +6,16 @@ function playGame() {
 
         switch (target.id) {
             case 'rock':
-                console.log("You clicked rock");
                 playRound("rock", getComputerChoice());
                 break;
             case 'paper':
-                console.log("You clicked paper");
                 playRound("paper", getComputerChoice());
                 break;
             case 'scissors':
-                console.log("You clicked scissors");
                 playRound("scissors", getComputerChoice());
                 break;
         }
     })
-
-    /*console.log("Total scores: You - " + humanScore + "; Computer - " + computerScrore);
-    if (humanScore > computerScrore) {
-        console.log("You won the game!");
-    }
-    else if (humanScore === computerScrore) {
-        console.log("It's a draw!");
-    }
-    else {
-        console.log("Computer won!");
-    }*/
 }
 
 function getComputerChoice() {
@@ -46,31 +32,44 @@ function getComputerChoice() {
     }
 }
 
-let humanScore = 0, computerScrore = 0;
+let humanScore = 0, computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
 
+    const p = document.getElementById("gameStart");
+    p.classList.add("inactive");
+
+    document.getElementById("humanChoice").textContent = humanChoice;
+    document.getElementById("computerChoice").textContent = computerChoice;
+
     if (humanChoice === "rock" && computerChoice === "rock" ||
         humanChoice === "paper" && computerChoice === "paper" ||
         humanChoice === "scissors" && computerChoice === "scissors") {
-        console.log("It's a draw! Your choice: " + humanChoice + " matches the computers: " + computerChoice + ".");
+
+        document.getElementById("roundResults").textContent = "It's a draw! Your choice: " + humanChoice + " matches the computers: " + computerChoice + ".";
     }
     else if (humanChoice === "rock" && computerChoice === "scissors" ||
         humanChoice === "scissors" && computerChoice === "paper" ||
         humanChoice === "paper" && computerChoice === "rock") {
         humanScore++;
-        console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+
+        document.getElementById("roundResults").textContent = "You win! " + humanChoice + " beats " + computerChoice + ".";
     }
     else {
-        computerScrore++;
-        console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+        computerScore++;
+
+        document.getElementById("roundResults").textContent = "You lose! " + computerChoice + " beats " + humanChoice + ".";
     }
 
-    // scores and updating them in UI to be done in this function
+    document.getElementById("humanScore").textContent = humanScore;
+    document.getElementById("computerScore").textContent = computerScore;
 }
 
 document.getElementById("start").addEventListener('click', function () {
-    console.log("Game Starts!");
+    document.getElementById("gameStart").textContent = "Game Starts!";
     playGame();
+
+    const div = document.getElementById("results");
+    div.classList.remove("inactive");
 })
