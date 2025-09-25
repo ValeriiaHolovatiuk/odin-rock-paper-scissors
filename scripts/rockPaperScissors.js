8b1e74be09557de7ffd95c692c1e424efd238676
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-09-26 00:34:06 +0200

    Add the End Game and Reset Game buttons; Added logic around End Game button; Added helper fuctions for actions done often (for example, Add or Remove class for element; Added CSS changes for the buttons;

--- scripts/rockPaperScissors.js
+++ scripts/rockPaperScissors.js
@@ -32,7 +32,11 @@ function getComputerChoice() {
     }
 }
 
-let humanScore = 0, computerScore = 0;
+function ResetValues(...values) {
+    return values.map(() => 0);
+}
+
+let humanScore = 0, computerScore = 0, totalRounds = 0;
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
@@ -47,17 +51,21 @@ function playRound(humanChoice, computerChoice) {
         humanChoice === "paper" && computerChoice === "paper" ||
         humanChoice === "scissors" && computerChoice === "scissors") {
 
+        totalRounds++;
+
         document.getElementById("roundResults").textContent = "It's a draw! Your choice: " + humanChoice + " matches the computers: " + computerChoice + ".";
     }
     else if (humanChoice === "rock" && computerChoice === "scissors" ||
         humanChoice === "scissors" && computerChoice === "paper" ||
         humanChoice === "paper" && computerChoice === "rock") {
         humanScore++;
+        totalRounds++;
 
         document.getElementById("roundResults").textContent = "You win! " + humanChoice + " beats " + computerChoice + ".";
     }
     else {
         computerScore++;
+        totalRounds++;
 
         document.getElementById("roundResults").textContent = "You lose! " + computerChoice + " beats " + humanChoice + ".";
     }
@@ -66,29 +74,83 @@ function playRound(humanChoice, computerChoice) {
     document.getElementById("computerScore").textContent = computerScore;
 }
 
-document.getElementById("start").addEventListener('click', function () {
-    document.getElementById("gameStart").textContent = "Game Starts!";
+const divResults = document.getElementById("results");
+const divGameButtons = document.getElementById("gameButtons");
+const startButton = document.getElementById("startGameButton");
+const endResetButtons = document.getElementById("gameStateButtons");
+const divEndGameResults = document.getElementById("endGameResults");
+const resetGameButton = document.getElementById("resetGameButton");
+const endGameButton = document.getElementById("endGameButton");
+
+function AddCSSClass(className, ...values) {
+    values.forEach(element => {
+        element.classList.add(className);
+    });
+}
+
+function RemoveCSSClass(className, ...values) {
+    values.forEach(element => {
+        element.classList.remove(className);
+    });
+}
+
+function UpdateElements(values) {
+    for (const [id, value] of Object.entries(values)) {
+        const el = document.getElementById(id);
+        if (el) el.textContent = value;
+    }
+}
+
+// Start Game button is clicked
+
+document.getElementById("startGameButton").addEventListener('click', function () {
+    AddCSSClass("inactive", divEndGameResults, startButton);
+
+    [humanScore, computerScore, totalRounds] = ResetValues(humanScore, computerScore, totalRounds);
+
+    UpdateElements({
+        humanChoice: "-", computerChoice: "-", roundResults: "", humanScore: humanScore, computerScore: computerScore, gameStart: "Game Starts!"
+    });
+
     playGame();
 
-    const divResults = document.getElementById("results");
-    divResults.classList.remove("inactive");
-
-    const divGameButtons = document.getElementById("gameButtons");
-    divGameButtons.classList.remove("inactive");
-
-    const startButton = document.getElementById("start");
-    startButton.classList.add("inactive");
+    RemoveCSSClass("inactive", divResults, divGameButtons, endResetButtons);
 
     /* 
         For the game logic I also need to add the following: End Game button, Reset Game button, and the logic around them
 
-        Logic around End Game button:
-         - removes all extra text - the only text left is total score + total rounds played (it requires extra logic added);
-         - Start Game button is displayed again;
+        End Game button logic is done
 
         Logic around Reset Game button:
          - all text is removed;
          - visually it looks like as if Start Game button was just clicked;
          - scores are set to 0;
+
+        UPD: I also would like to add posibility to revert to initial screen look (only text + start game button) if the Welcome text is clicked
     */
+})
+
+// End Game button is clicked
+
+document.getElementById("endGameButton").addEventListener('click', function () {
+    AddCSSClass("inactive", divResults, divGameButtons, endResetButtons);
+    RemoveCSSClass("inactive", divEndGameResults, startButton);
+
+    let tempHumanScore = humanScore, tempCompScore = computerScore, tempTotalRounds = totalRounds;
+
+    [humanScore, computerScore, totalRounds] = ResetValues(humanScore, computerScore, totalRounds);
+
+    UpdateElements({ totalRounds: tempTotalRounds, humanScoreEnd: tempHumanScore, computerScoreEnd: tempCompScore, gameStart: "" });
+
+    if (tempCompScore > tempHumanScore) {
+        UpdateElements({ allRoundsWinner: "All rounds winner is Computer!" })
+    }
+    else if (tempCompScore < tempHumanScore) {
+        UpdateElements({ allRoundsWinner: "All rounds winner is You!" })
+    }
+    else {
+        UpdateElements({ allRoundsWinner: "Unbelivable! It's a draaaaw!" })
+    }
+
+    [tempHumanScore, tempCompScore, tempTotalRounds] = ResetValues(tempHumanScore, tempCompScore, tempTotalRounds);
 })
